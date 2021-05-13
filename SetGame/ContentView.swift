462c69a95ce6f883a082a7db42c61a643e94a19d
Luda <fuxlud@gmail.com>
2021-05-13 22:54:22 +0300

    Initial Commit

---
+++ SetGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by Admin on 13/05/2021.
+//  Copyright © 2021 fuxlud. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
