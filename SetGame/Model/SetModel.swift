2bb6d82ea90cda46d31405fc90599a436b5bd033
Luda <fuxlud@gmail.com>
2021-05-18 19:44:24 +0300

    removing prints

--- SetGame/Model/SetModel.swift
+++ SetGame/Model/SetModel.swift
@@ -28,12 +28,13 @@ class SetGame {
     }
     
     func select(_ card: Card) {
-        if (selectedCards.count > 3) { //MARK: to delete
-            turnDownAllSelectedCards()
-        }
         
         card.select()
         selectedCards.append(card)
+        
+        if (selectedCards.count > 3) { //MARK: to delete
+            turnDownAllSelectedCards()
+        }
         
         if (selectedCards.count == 3) {
             let isSet = checkIfSet()
@@ -41,12 +42,10 @@ class SetGame {
             if isSet {
                 for card in selectedCards{
                     card.markAsMatch()
-                    print(card)
                 }
             } else {
                 for card in selectedCards{
                     card.markAsNotMatched()
-                    print(card)
                 }
             }
         }
@@ -64,35 +63,19 @@ class SetGame {
         
         let equalShading = (card1.shading == card2.shading) && (card2.shading == card3.shading)
         let allDifferentShading = (card1.shading != card2.shading) && (card2.shading != card3.shading) && (card1.shading != card3.shading)
-        print("equalShading - \(equalShading)")
-        print("allDifferentShading - \(allDifferentShading)")
         let shading = equalShading || allDifferentShading
-        print("shading - \(shading)")
-        print("----------")
         
         let equalColor = (card1.color == card2.color) && (card2.color == card3.color)
         let allDifferentColor = (card1.color != card2.color) && (card2.color != card3.color) && (card1.color != card3.color)
-        print("equalColor - \(equalColor)")
-        print("allDifferentColor - \(allDifferentColor)")
         let color = equalColor || allDifferentColor
-        print("color - \(color)")
-        print("----------")
         
         let equalShade = (card1.shape == card2.shape) && (card2.shape == card3.shape)
         let allDifferentShade = (card1.shape != card2.shape) && (card2.shape != card3.shape) && (card1.shape != card3.shape)
-        print("equalColor - \(equalColor)")
-        print("allDifferentColor - \(allDifferentColor)")
         let shade = equalShade || allDifferentShade
-        print("shade - \(shade)")
-        print("----------")
         
         let equalNumberOfShapes = (card1.numberOfShapes == card2.numberOfShapes) && (card2.numberOfShapes == card3.numberOfShapes)
         let allDifferentNumberOfShapes = (card1.numberOfShapes != card2.numberOfShapes) && (card2.numberOfShapes != card3.numberOfShapes) && (card1.numberOfShapes != card3.numberOfShapes)
-        print("equalNumberOfShapes - \(equalNumberOfShapes)")
-        print("allDifferentNumberOfShapes - \(allDifferentNumberOfShapes)")
         let numberOfShapes = equalNumberOfShapes || allDifferentNumberOfShapes
-        print("shape - \(numberOfShapes)")
-        print("----------")
         
         let isSet = shading && color && shade && numberOfShapes
         print(isSet)
